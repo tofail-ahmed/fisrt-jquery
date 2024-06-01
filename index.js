013b4882f7a4268ba3e4d5698c7a3d5c0f4d89c9
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-01 12:38:34 +0600

    append() and prepned() added

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 // $('h1').text("Assalamualaikum JQuery");
 
 $(".my-div h1,h1 ").text("Asslamualaikum")
-$("#p1").html("<h1 style='color:red;font-size:40px;' ><b><i>Walaikumslam<i/><b/><h1/>")
+// $("#p1").html("<h1 style='color:red;font-size:40px;' ><b><i>Walaikumslam<i/><b/><h1/>")
+$("#p1").prepend("<p style='color:green; font-size:20px;'>areh...<p>")
+$("#p1").append("  <p>kemon aso???<p/>")
