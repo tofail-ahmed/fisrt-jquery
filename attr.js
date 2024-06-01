b2a900fc0f2cf63c011f812114eb7a9b9cb5fcae
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-01 14:11:58 +0600

    attribute method added

---
+++ attr.js
@@ -0,0 +1,13 @@
+// var myAttr=document.querySelector('a').getAttribute('href')
+// console.log(myAttr)
+
+
+// $('a').removeAttr('href')
+
+
+
+// document.querySelector('a').setAttribute('href','https://atofail.vercel.app')
+
+$('a').attr('href','https://atofail.vercel.app')
+var myAttr1=$('a').attr('href')
+console.log(myAttr1)
