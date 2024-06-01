9a9c187cb7d8d1f1c9c6fe00a9b31697d86df0ad
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-01 12:29:58 +0600

    text() and html() method added

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // document.getElementById("jq").innerHTML="hi";
-$('h1').text("Assalamualaikum JQuery");
+// $('h1').text("Assalamualaikum JQuery");
+
+$(".my-div h1,h1 ").text("Asslamualaikum")
+$("#p1").html("<h1 style='color:red;font-size:40px;' ><b><i>Walaikumslam<i/><b/><h1/>")
