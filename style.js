2f39b1e46e3e88c8b9a45f423aa6f7184be62463
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-01 15:27:37 +0600

    css manipulation added

---
+++ style.js
@@ -0,0 +1,21 @@
+// $('#p1').css('color','green')
+// $('#p1').css('font-size','24px')
+// $('#p1').css('font-weight','900')
+// $('#p1').css('font-style','italic')
+
+// $('#p1').css({
+// 'color':"maroon",
+// 'font-size':'30px',
+// 'font-weight':"900",
+// 'font-style':'italic',
+
+// })
+
+// document.querySelector("#p1").classList.add("style1")
+// $("#p1").addClass('style1 ')
+// $("#p1").addClass(' style2')
+$("#p1").addClass('style1 style2')
+
+
+
+//! addClass, removeClass, 
